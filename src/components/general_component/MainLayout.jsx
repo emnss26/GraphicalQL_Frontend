2c385d7ab9e3dcb53920790e23f16ca6868f950e
Enrique Meneses <109693762+emnss26@users.noreply.github.com>
2025-06-17 20:48:42 -0600

    Adjust layout colors and page layouts

--- src/components/general_component/MainLayout.jsx
+++ src/components/general_component/MainLayout.jsx
@@ -4,7 +4,7 @@ import MainSideBar from "@/components/general_component/MainSideBar";
 export default function MainLayout({ children }) {
   return (
     <div className="flex flex-col h-screen overflow-hidden bg-white">
-      <header className="bg-gray-500 text-white p-4 flex justify-between items-center">
+      <header className="bg-[rgb(75,80,86)] text-white p-4 flex justify-between items-center">
         <span>Abitat Construction Solutions</span>
         <User className="w-6 h-6" />
       </header>
@@ -12,7 +12,7 @@ export default function MainLayout({ children }) {
         <MainSideBar />
         <main className="flex-1 overflow-auto p-4">{children}</main>
       </div>
-      <footer className="bg-red-600 bg-opacity-50 text-white p-2 flex justify-end">
+      <footer className="bg-[rgb(170,32,47)] text-white p-2 flex justify-end">
         <span>Abitat Construction Solutions</span>
       </footer>
     </div>
